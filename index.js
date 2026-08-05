cefc3db61211798983112050b0f31bb7ac5595f4
nvenkatvarshith <venkatvarshith2@gmail.com>
2026-08-05 16:48:54 +0530

    added input validation, button on hover

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ let flashcard = [];
 (function() {
     flashcard = JSON.parse(localStorage.getItem('flashcard') || '[]');
     let str="";
-    flashcard.forEach(flashcard => {
+    flashcard.forEach((flashcard,index) => {
         str += `
-            <div class="card py-4 text-center" style="width: 13.2rem;">
+            <div class="card py-4 text-center" id="${index}" style="width: 13.2rem;" onmouseenter="showCategoryOptions(${index})" onmouseleave="hideCategoryOptions(${index})">
                 <div class="card-body">
                     <h4>${flashcard.category}</h4>
+                    <div class="d-none" id="showbutton-${index}">
+                        <button type="button" class="btn btn-outline-secondary px-4" data-bs-toggle="modal" data-bs-target="#exampleModal">view</button>
+                        
+                    </div>
                 </div>
             </div>
         `
@@ -17,7 +21,22 @@ let flashcard = [];
 
 function addNewCategory(){
     const category = document.getElementById("newcategory");
-    flashcard.push({category: category.value});
-    localStorage.setItem("flashcard", JSON.stringify(flashcard));
-    window.location.href = "/";
+    if(category.value == ''){
+        document.getElementById("error-msg").innerHTML = "Please enter the value";
+    }else{
+        flashcard.push({category: category.value.toUpperCase()});
+        localStorage.setItem("flashcard", JSON.stringify(flashcard));
+        window.location.href = "/";
+    }
+}
+
+function showCategoryOptions(cardid){
+    let showButtons = document.getElementById("showbutton-"+cardid).classList;
+    if(showButtons.value.includes("d-none")){
+        showButtons.remove("d-none");
+    }
+}
+
+function hideCategoryOptions(cardid){
+    document.getElementById("showbutton-"+cardid).classList.add("d-none");
 }
