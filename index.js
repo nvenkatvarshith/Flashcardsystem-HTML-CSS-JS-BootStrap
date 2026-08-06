00b2200601a144d9238b393af78a236605dc42d4
nvenkatvarshith <venkatvarshith2@gmail.com>
2026-08-06 14:05:53 +0530

    completed code changes for add card functionality

--- index.js
+++ index.js
@@ -17,7 +17,31 @@ let studydeck = [];
                         <div class="d-grid gap-2">
                             <button id="flashcard-${index}" type="button" class="btn btn-success w-100 mt-2 px-4 py-2" onclick = "showDeckCards('${flashcard.categoryname}')">Study Now</button>
                             <div class="btn-group gap-2" role="group" aria-label="Basic example">
-                                <button type="button" class="btn btn-outline-secondary">Add Card</button>
+                                <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                                    Add Card
+                                </button>
+
+                                <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Add Card</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form>
+                                                    <input type="text" class="w-100 py-2 rounded-3" name="front" id="front${flashcard.categoryname}" placeholder="Enter front of card">
+                                                    <input type="text" class="w-100 py-2 mt-2 rounded-3" name="back" id="back${flashcard.categoryname}" placeholder="Enter back of card">
+                                                    <input type="number" class="w-100 py-2 mt-2 rounded-3" name="interval" id="interval${flashcard.categoryname}" placeholder="Enter interval">
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-success" onclick = "addCardToCategory('${flashcard.categoryname}')">Add Card</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <button type="button" class="btn btn-light">AI Generate✨</button>
                             </div>
                         </div>
@@ -85,4 +109,24 @@ function populateFlashCards(selectedCategory,index){
 function revealAnswer(flashcardid){
     document.getElementById('back'+flashcardid).classList.remove('d-none');
     document.getElementById('front'+flashcardid).classList.add('d-none');
+}
+
+function addCardToCategory(categoryname){
+    let front = document.getElementById("front"+categoryname).value;
+    let back = document.getElementById("back"+categoryname).value;
+    let interval = document.getElementById("interval"+categoryname).value;
+    let card = {
+        front,
+        back,
+        interval,
+        nextReviewDate: new Date().toISOString
+    };
+    studydeck.forEach((deck) => {
+        if(deck.categoryname == categoryname) {
+            card['id'] = 101 + deck.flashcards.length;
+            deck.flashcards.push(card);
+        }
+    });
+    localStorage.setItem("studydeck", JSON.stringify(studydeck));
+    window.location.href = "/";
 }
