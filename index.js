f96c21240816239fc73f94fbc8d86f1cafb7fe03
nvenkatvarshith <venkatvarshith2@gmail.com>
2026-08-06 13:36:51 +0530

    completed code changes for Study now button

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-let flashcard = [];
+let studydeck = [];
 
 (function() {
-    flashcard = JSON.parse(localStorage.getItem('flashcard') || '[]');
+    studydeck = JSON.parse(localStorage.getItem('studydeck') || '[]');
     let str="";
-    flashcard.forEach((flashcard,index) => {
+    studydeck.forEach((flashcard,index) => {
         str += `
             <div class="col mt-2">
                 <div class="card py-4 text-center" id="${index}">
                     <div class="card-body">
-                        <h4>${flashcard.category} ${showCardStatus(flashcard.cardsdue)}</h4>
+                        <h4>${flashcard.categoryname} ${showCardStatus(flashcard.cardsdue)}</h4>
                         <div class="d-flex flex-column fw-semibold">
                             <div class="p-1">Total Cards:</div>
                             <div class="p-1">Learning:</div>
                             <div class="p-1">Graduated: </div>
                         </div>
                         <div class="d-grid gap-2">
-                            <button id="study-${index}" type="button" class="btn btn-success w-100 mt-2 px-4 py-2" data-bs-toggle="modal" data-bs-target="#exampleModal">Study Now</button>
+                            <button id="flashcard-${index}" type="button" class="btn btn-success w-100 mt-2 px-4 py-2" onclick = "showDeckCards('${flashcard.categoryname}')">Study Now</button>
                             <div class="btn-group gap-2" role="group" aria-label="Basic example">
                                 <button type="button" class="btn btn-outline-secondary">Add Card</button>
                                 <button type="button" class="btn btn-light">AI Generate✨</button>
@@ -34,8 +34,8 @@ function addNewCategory(){
     if(category.value == ''){
         document.getElementById("error-msg").innerHTML = "Please enter the value";
     }else{
-        flashcard.push({category: category.value.toUpperCase()});
-        localStorage.setItem("flashcard", JSON.stringify(flashcard));
+        studydeck.push({category: category.value.toUpperCase()});
+        localStorage.setItem("studydeck", JSON.stringify(studydeck));
         window.location.href = "/";
     }
 }
@@ -46,4 +46,43 @@ function showCardStatus(cardsdue){
    }else{
         return `<span class="badge text-bg-success">No due</span>`;
    }
+}
+
+function showDeckCards(selectedCategory){
+    console.log(selectedCategory);
+    studydeck.forEach((studydeckcategory,index) => {
+        if(studydeckcategory.categoryname === selectedCategory){
+            document.getElementById("show-categories").classList.add("d-none");
+            populateFlashCards(selectedCategory,index);
+            document.getElementById("flashcards").classList.remove("d-none");
+        }
+    })
+}
+
+function populateFlashCards(selectedCategory,index){
+    let str = "";
+    console.log(studydeck[index].flashcards);
+    studydeck[index].flashcards.forEach((flashcard) => {
+        str += `
+            <div class="card col" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${selectedCategory}</h5>
+                    ${` <div id="front${flashcard.id}">
+                            <h6 class="card-subtitle mb-2 text-body-secondary">${flashcard.front}</h6>
+                            <button type="button" class="btn btn-outline-secondary" onclick="revealAnswer('${flashcard.id}')">Reveal Answer</button>
+                        </div>
+                        <div id="back${flashcard.id}" class="d-none mt-3">
+                            <h6 class="card-subtitle mb-2 text-body-secondary">Answer: ${flashcard.back}</h6>
+                        </div>
+                    `} 
+                </div>
+            </div>
+        `;
+    });
+    document.getElementById("flashcards-deck").innerHTML = str;
+}
+
+function revealAnswer(flashcardid){
+    document.getElementById('back'+flashcardid).classList.remove('d-none');
+    document.getElementById('front'+flashcardid).classList.add('d-none');
 }
